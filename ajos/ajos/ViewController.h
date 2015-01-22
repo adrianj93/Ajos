a498601a576bb9a1cd095761711357b37979a2d7
Adrian Janiak <adrian@Adrians-Mac.local>
2015-01-22 17:57:22 +0100

    Project initialization

---
+++ ajos/ajos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ajos
+//
+//  Created by Adrian Janiak on 22.01.2015.
+//  Copyright (c) 2015 Adrian Janiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ajos/ajosTests/ajosTests.m
@@ -0,0 +1,40 @@
+//
+//  ajosTests.m
+//  ajosTests
+//
+//  Created by Adrian Janiak on 22.01.2015.
+//  Copyright (c) 2015 Adrian Janiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ajosTests : XCTestCase
+
+@end
+
+@implementation ajosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
