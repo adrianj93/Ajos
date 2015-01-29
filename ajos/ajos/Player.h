8611e4206f102e36b3c8ffea0b3aea21987e1601
endablju <szymonrz@Szymons-Mac.local>
2015-01-29 22:42:25 +0100

    wyglad
    chyba taki wyglad bedzie ok, powiedz czy u ciebie wszystko działa i
    możesz troche grafiki wtsawić

---
+++ ajos/ajos/Player.h
@@ -0,0 +1,17 @@
+//
+//  Player.h
+//  ajos
+//
+//  Created by Szymon Rz on 28/01/15.
+//  Copyright (c) 2015 Adrian Janiak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Player : NSManagedObject
+
+@property (nonatomic, retain) NSString * nick;
+
+@end
---
+++ ajos/ajos/Player.m
@@ -0,0 +1,16 @@
+//
+//  Player.m
+//  ajos
+//
+//  Created by Szymon Rz on 28/01/15.
+//  Copyright (c) 2015 Adrian Janiak. All rights reserved.
+//
+
+#import "Player.h"
+
+
+@implementation Player
+
+@dynamic nick;
+
+@end
